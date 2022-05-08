aac03e00995e218577f9f4b7be5e1c96b2bd0f43
LahiruRJ <sanlahiru720@gmail.com>
2022-05-08 16:34:59 +0530

    Add basic initializers for password controller class

---
+++ src/controller/ForgotPasswordFormController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordFormController {
+    public TextField txtForgotPasswordHint;
+    public Button btnforgotPasswordOk;
+    public Label correctPassword;
+
+    public void btnforgotPasswordOk_OnAction(ActionEvent event) {
+
+    }
+}
